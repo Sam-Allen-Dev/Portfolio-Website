1babbf366c0674e62fe70ab2774631ae3db9a79b
Samuel <allen.samuel.business@gmail.com>
2022-03-03 18:46:33 +0800

    Replaced linked in link with github link; fixed body and footer formatting; centered items in drop down list;

--- JavaScript/PageStructure.js
+++ JavaScript/PageStructure.js
@@ -17,14 +17,20 @@ function createNav()
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" style="color: black;" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Connect</a>
           <div class="dropdown-menu">
-            <a class="dropdown-item" href="mailto:allen.samuel.business@gmail.com">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg" style="width: 20%;">
-              &nbsp;&nbsp;Gmail
+            <a class="dropdown-item" href="mailto:allen.samuel.business@gmail.com" style="display: flex; align-items: center;">
+              <img src="https://img.icons8.com/fluency-systems-filled/144/000000/ms-outlook.png" style="width: 20%;">
+              &nbsp;&nbsp;Email
             </a>
-            <a class="dropdown-item" href="#">
+            <a class="dropdown-item" href="https://github.com/Sam-Allen-Dev" target="blank_" style="display: flex; align-items: center;">
+              <img src="./PNG/GitHub Logo Black Transparent.png" style="width: 20%;">
+              &nbsp;&nbsp;GitHub
+            </a>
+	    <!--
+            <a class="dropdown-item" href="#" style="display: flex; align-items: center;">
               <img src="./SVG/linkedIn.svg" style="width: 20%;">
               &nbsp;&nbsp;LinkedIn
             </a>
+	    -->
           </div>
         </li>
       </ul>
@@ -38,8 +44,7 @@ function createFooter()
 {
   document.write
   (`
-    <footer>
-      <br><br><br><br><br><br><br><br>
+    <footer style="position: relative; min-height: 20%; margin-top:20%; padding: 0; margin-bottom: 0;">
 
       <p style="text-align: right;">
         Portfolio 2021
@@ -50,7 +55,6 @@ function createFooter()
           info
         </a>
       </p>
-      <br><br>
     </footer>
   `);
 }
